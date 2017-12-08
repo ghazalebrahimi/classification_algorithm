31a5b5b5ab66b6cfd73c1793eae24329673d4a7d
Reza Soltani <rezas@divar.ir>
2017-12-08 19:18:54 +0330

    Added gradient boostring classifier

--- gradient_boosting_classifier.py
+++ gradient_boosting_classifier.py
@@ -1,17 +1,17 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 
-def random_forest_classifier(train_set, valid_set, test_set):
+def gradient_boosting_classifier(train_set, valid_set, test_set):
 
-    clf = RandomForestClassifier()
-    clf.fit(train_set[0], train_set[1])
+    clf = GradientBoostingClassifier()
+    clf.fit(train_set[0][:1000], train_set[1][:1000])
 
     valid_predict = clf.predict(valid_set[0])
     wrong_answer = 0
     for i, j in zip(valid_predict, valid_set[1]):
         if i != j:
             wrong_answer += 1
-    print("Random Forest Classifier predicts the validation set with accuracy of: ",
+    print("Gradient Boosting Classifier predicts the validation set with accuracy of: ",
           100 * (len(valid_predict) - wrong_answer) / (len(valid_predict)))
 
     test_predict = clf.predict(test_set[0])
@@ -19,5 +19,5 @@ def random_forest_classifier(train_set, valid_set, test_set):
     for i, j in zip(test_predict, test_set[1]):
         if i != j:
             wrong_answer += 1
-    print("Random Forest Classifier predicts the validation set with accuracy of:  ",
+    print("Gradient Boosting Classifier predicts the test set with accuracy of: ",
           100 * (len(test_predict) - wrong_answer) / (len(test_predict)))
--- starter.py
+++ starter.py
@@ -1,4 +1,6 @@
 import pickle, gzip, numpy
+
+from gradient_boosting_classifier import gradient_boosting_classifier
 from random_forest_classifier import random_forest_classifier
 
 f = gzip.open('mnist.pkl.gz', 'rb')
@@ -7,3 +9,4 @@ f.close()
 
 
 random_forest_classifier(train_set, valid_set, test_set)
+gradient_boosting_classifier(train_set, valid_set, test_set)
