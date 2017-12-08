c41feef450d262f70abcc90f2c1aa88c40773092
Reza Soltani <rezas@divar.ir>
2017-12-08 18:14:01 +0330

    Added Random forest classifier

---
+++ random_forest_classifier.py
@@ -0,0 +1,23 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+def random_forest_classifier(train_set, valid_set, test_set):
+
+    clf = RandomForestClassifier()
+    clf.fit(train_set[0], train_set[1])
+
+    valid_predict = clf.predict(valid_set[0])
+    wrong_answer = 0
+    for i, j in zip(valid_predict, valid_set[1]):
+        if i != j:
+            wrong_answer += 1
+    print("Random Forest Classifier prediction accuracy for validation set is: ",
+          100 * (len(valid_predict) - wrong_answer) / (len(valid_predict)))
+
+    test_predict = clf.predict(test_set[0])
+    wrong_answer = 0
+    for i, j in zip(test_predict, test_set[1]):
+        if i != j:
+            wrong_answer += 1
+    print("Random Forest Classifier prediction accuracy for test set is: ",
+          100 * (len(test_predict) - wrong_answer) / (len(test_predict)))
--- starter.py
+++ starter.py
@@ -1,6 +1,9 @@
 import pickle, gzip, numpy
+from random_forest_classifier import random_forest_classifier
 
-# Load the dataset
 f = gzip.open('mnist.pkl.gz', 'rb')
 train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 f.close()
+
+
+random_forest_classifier(train_set, valid_set, test_set)
