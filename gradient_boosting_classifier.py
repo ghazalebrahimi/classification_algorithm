3931846acdcbc1e1ccaa53d7cc7c31ef90698902
Reza Soltani <rezas@divar.ir>
2018-01-01 21:48:14 +0330

    Accuracy calculator refactored.

--- gradient_boosting_classifier.py
+++ gradient_boosting_classifier.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
 
 
 def gradient_boosting_classifier(train_set, valid_set, test_set):
@@ -7,17 +8,9 @@ def gradient_boosting_classifier(train_set, valid_set, test_set):
     clf.fit(train_set[0][:1000], train_set[1][:1000])
 
     valid_predict = clf.predict(valid_set[0])
-    wrong_answer = 0
-    for i, j in zip(valid_predict, valid_set[1]):
-        if i != j:
-            wrong_answer += 1
-    print("Gradient Boosting Classifier predicts the validation set with accuracy of: ",
-          100 * (len(valid_predict) - wrong_answer) / (len(valid_predict)))
+    print ("Gradient Boosting Classifier predicts the validation set with accuracy of:  :",
+           accuracy_score(valid_predict, valid_set[1]))
 
     test_predict = clf.predict(test_set[0])
-    wrong_answer = 0
-    for i, j in zip(test_predict, test_set[1]):
-        if i != j:
-            wrong_answer += 1
-    print("Gradient Boosting Classifier predicts the test set with accuracy of: ",
-          100 * (len(test_predict) - wrong_answer) / (len(test_predict)))
+    print ("Gradient Boosting Classifier predicts the test set with accuracy of: ",
+           accuracy_score(test_predict, test_set[1]))
--- random_forest_classifier.py
+++ random_forest_classifier.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 
 def random_forest_classifier(train_set, valid_set, test_set):
@@ -7,17 +8,9 @@ def random_forest_classifier(train_set, valid_set, test_set):
     clf.fit(train_set[0], train_set[1])
 
     valid_predict = clf.predict(valid_set[0])
-    wrong_answer = 0
-    for i, j in zip(valid_predict, valid_set[1]):
-        if i != j:
-            wrong_answer += 1
-    print("Random Forest Classifier predicts the validation set with accuracy of: ",
-          100 * (len(valid_predict) - wrong_answer) / (len(valid_predict)))
+    print ("Random Forest Classifier predicts the validation set with accuracy of:  :",
+           accuracy_score(valid_predict, valid_set[1]))
 
     test_predict = clf.predict(test_set[0])
-    wrong_answer = 0
-    for i, j in zip(test_predict, test_set[1]):
-        if i != j:
-            wrong_answer += 1
-    print("Random Forest Classifier predicts the validation set with accuracy of:  ",
-          100 * (len(test_predict) - wrong_answer) / (len(test_predict)))
+    print ("Random Forest Classifier predicts the test set with accuracy of: ",
+           accuracy_score(test_predict, test_set[1]))
